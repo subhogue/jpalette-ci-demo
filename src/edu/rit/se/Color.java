96b9b49c7c2ed648013f2db691aafcbbeecb1990
Dylan M. Hogue <dmh6258@rit.edu>
2014-11-20 11:29:52 -0500

    first push...added a space

--- src/edu/rit/se/Color.java
+++ src/edu/rit/se/Color.java
@@ -5,7 +5,7 @@ package edu.rit.se;
  * A common interface all colors share, allowing a generic color to be 
  * transformed in its backend representation.
  * </p>
- * 
+ *  
  * @author Steven Kroh (skk8768)
  */
 public interface Color
